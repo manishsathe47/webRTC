37bf2fdaef020bfe79f46207af1713d845cd5372
Manish Sathe <64087712+manishsathe47@users.noreply.github.com>
2023-12-07 00:07:36 +0530

    day2

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express();
-
-const userController = require('../controllers/userController');
-
-routes.get('/',userController.loadIndex);
-
-module.exports = express.Router;
